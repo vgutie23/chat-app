f0b947ec6c28320703c83c9f7035a8b3637b8b2d
Vanessa Gutierrez <vanessagutierrez93@msn.com>
2021-04-21 22:19:15 -0500

    Created Home Page for No Auth

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
   <NavBar />
+  <Messages v-if="isAuthenticated" />
+  <NoAuth v-else />
 </template>
+
+<script setup>
+import { authentication } from '~/helpers/useFirebase'
+
+const { isAuthenticated } = authentication()
+</script>
---
+++ src/components/NoAuth.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col justify-center items-center space-y-5">
+    <flat-color-icons:lock class="w-36 h-36 mt-40 opacity-95" />
+    <h1 class="text-gray-400 text-4xl font-light tracking-wider">
+      Please Login to Chat
+    </h1>
+  </div>
+</template>
